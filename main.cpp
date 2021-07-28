b78712642c558e06aa9dcfbdfda6d3ddf39fa11b
SpudY2K <87917486+SpudY2K@users.noreply.github.com>
2021-07-28 23:17:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -880,6 +880,7 @@ void search_normals() {
 				plat.normal = { (float)nx, (float)ny, (float)nz };
 
 				double T_diff01 = T_tilt[0][1] - T_start[0][1];
+				double T_diff11 = T_tilt[1][1] - T_start[1][1];
 				double T_diff21 = T_tilt[2][1] - T_start[2][1];
 				double r_min = lower_y - (1 + T_diff11)*max_y + T_diff01 * plat.pos[0] + T_diff11 * plat.pos[1] + T_diff21 * plat.pos[2];
 				double r_max = upper_y - (1 + T_diff11)*min_y + T_diff01 * plat.pos[0] + T_diff11 * plat.pos[1] + T_diff21 * plat.pos[2];
