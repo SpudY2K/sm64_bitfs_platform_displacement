e17f5d66d797b96efb44fde038581df35ab27c40
SpudY2K <87917486+SpudY2K@users.noreply.github.com>
2021-08-19 08:33:37 +0100

    Moved find_floor to Magic.hpp

--- Magic.hpp
+++ Magic.hpp
@@ -5,6 +5,7 @@
 #include <stdexcept>
 #include <utility>
 #include "vmath.hpp"
+#include "Surface.hpp"
 
 using namespace std;
 
@@ -16,5 +17,6 @@ pair<int16_t, float> calc_intended_yawmag(int8_t stick_x, int8_t stick_y);
 bool check_inbounds(const Mario &m);
 float dist_calc(const Vec3f& x, const Vec3f& y);
 float line_point(const Vec3s& p1, const Vec3s& p2, float x, bool yOrZ);
+Surface const * find_floor(Vec3f& pos, Vec2S& triangles);
 
 #endif
