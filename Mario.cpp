f70ad78882939d42636c35b9a1f843b7b472fc5c
SpudY2K <87917486+SpudY2K@users.noreply.github.com>
2021-08-19 08:37:20 +0100

    Updated ground_step to use normals from floor triangles

--- Mario.cpp
+++ Mario.cpp
@@ -4,14 +4,25 @@ void Mario::set_pos(const Vec3f& position) {
     this->pos = position;
 }
 
-int Mario::ground_step(int yaw, float normalY) {
+int Mario::ground_step(int yaw, Vec2S& triangles) {
     int steps = 0;
 
     for (int i = 0; i < 4; i++) {
-        this->pos[0] = this->pos[0] + gSineTable[uint16_t(yaw) >> 4] * normalY * (this->speed / 4.0f);
-        this->pos[2] = this->pos[2] + gCosineTable[uint16_t(yaw) >> 4] * normalY * (this->speed / 4.0f);
+		const Surface* floor = find_floor(pos, triangles);
 
-        if (!check_inbounds(*this)) { break; }
+		if (!floor) { break; }
+
+		float old_x = this->pos[0];
+		float old_z = this->pos[2];
+
+        this->pos[0] = this->pos[0] + gSineTable[uint16_t(yaw) >> 4] * floor->normal[1] * (this->speed / 4.0f);
+        this->pos[2] = this->pos[2] + gCosineTable[uint16_t(yaw) >> 4] * floor->normal[1] * (this->speed / 4.0f);
+
+        if (!check_inbounds(*this)) { 
+			this->pos[0] = old_x;
+			this->pos[2] = old_z;
+			break;
+		}
 
         steps++;
     }
