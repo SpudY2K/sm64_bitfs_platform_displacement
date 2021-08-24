7a3eb8f80bad810bd8069f29ae0fb0f4d1765eb9
SpudY2K <87917486+SpudY2K@users.noreply.github.com>
2021-08-24 10:46:16 +0100

    Added yaw to Mario class

--- Mario.hpp
+++ Mario.hpp
@@ -14,16 +14,18 @@ class Mario
 public:
 	Vec3f pos = { 0,0,0 };
 	float speed;
+	int yaw;
 
 	Mario() {}
 
-	Mario(const Vec3f& position, float spd) {
+	Mario(const Vec3f& position, float spd, int yw) {
 		set_pos(position);
 		speed = spd;
+		yaw = yw;
 	}
 
 	void set_pos(const Vec3f& position);
-	int ground_step(int yaw, Vec2S& triangles);
+	int ground_step(Vec2S& triangles);
 };
 
 #endif
