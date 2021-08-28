c7437b4ae815ceb9795b000fb94009ccc1950647
SpudY2K <87917486+SpudY2K@users.noreply.github.com>
2021-08-28 01:59:34 +0100

    Updated triangle coordinate representation
    Replaced 3 coordinate variables with a single vector of coordinates

--- Surface.cpp
+++ Surface.cpp
@@ -12,9 +12,9 @@ void Surface::rotate(const Vec3s& pivot, const Mat4& old_mat, const Mat4& new_ma
 	Vec3f posAfterRotation3;
 
 	for(int i = 0; i < 3; i++) {
-		dist1[i] = vector1[i] - pivot[i];
-		dist2[i] = vector2[i] - pivot[i];
-		dist3[i] = vector3[i] - pivot[i];
+		dist1[i] = vectors[0][i] - pivot[i];
+		dist2[i] = vectors[1][i] - pivot[i];
+		dist3[i] = vectors[2][i] - pivot[i];
 	}
 
 	linear_mtxf_mul_vec3f(posBeforeRotation1, old_mat, dist1);
@@ -27,25 +27,25 @@ void Surface::rotate(const Vec3s& pivot, const Mat4& old_mat, const Mat4& new_ma
 	linear_mtxf_mul_vec3f(posAfterRotation3, new_mat, dist3);
 
 	for(int i = 0; i < 3; i++) {
-		vector1[i] += posAfterRotation1[i] - posBeforeRotation1[i];
-		vector2[i] += posAfterRotation2[i] - posBeforeRotation2[i];
-		vector3[i] += posAfterRotation3[i] - posBeforeRotation3[i];	
+		vectors[0][i] += posAfterRotation1[i] - posBeforeRotation1[i];
+		vectors[1][i] += posAfterRotation2[i] - posBeforeRotation2[i];
+		vectors[2][i] += posAfterRotation3[i] - posBeforeRotation3[i];
 	}
 
-	normal[0] = (((vector2[1] - vector1[1]) * (vector3[2] - vector2[2])) - ((vector2[2] - vector1[2]) * (vector3[1] - vector2[1]))) * 1000.0f;
-	normal[1] = (((vector2[2] - vector1[2]) * (vector3[0] - vector2[0])) - ((vector2[0] - vector1[0]) * (vector3[2] - vector2[2]))) * 1000.0f;
-	normal[2] = (((vector2[0] - vector1[0]) * (vector3[1] - vector2[1])) - ((vector2[1] - vector1[1]) * (vector3[0] - vector2[0]))) * 1000.0f;
+	normal[0] = (((vectors[1][1] - vectors[0][1]) * (vectors[2][2] - vectors[1][2])) - ((vectors[1][2] - vectors[0][2]) * (vectors[2][1] - vectors[1][1]))) * 1000.0f;
+	normal[1] = (((vectors[1][2] - vectors[0][2]) * (vectors[2][0] - vectors[1][0])) - ((vectors[1][0] - vectors[0][0]) * (vectors[2][2] - vectors[1][2]))) * 1000.0f;
+	normal[2] = (((vectors[1][0] - vectors[0][0]) * (vectors[2][1] - vectors[1][1])) - ((vectors[1][1] - vectors[0][1]) * (vectors[2][0] - vectors[1][0]))) * 1000.0f;
 
 	vec3f_normalize(normal);
 
-	originOffset = -(normal[0] * vector1[0] + normal[1] * vector1[1] + normal[2] * vector1[2]);
+	originOffset = -(normal[0] * vectors[0][0] + normal[1] * vectors[0][1] + normal[2] * vectors[0][2]);
 }
 
 void Surface::repr() {
 	printf("vector1: (");
 
 	for (int i = 0; i < 3; i++) {
-		printf("%d", vector1[i]);
+		printf("%d", vectors[0][i]);
 
 		if (i != 2) {
 			printf(", ");
@@ -58,7 +58,7 @@ void Surface::repr() {
 	printf("vector2: (");
 
 	for (int i = 0; i < 3; i++) {
-		printf("%d", vector2[i]);
+		printf("%d", vectors[1][i]);
 
 		if (i != 2) {
 			printf(", ");
@@ -71,7 +71,7 @@ void Surface::repr() {
 	printf("vector3: (");
 
 	for (int i = 0; i < 3; i++) {
-		printf("%d", vector3[i]);
+		printf("%d", vectors[2][i]);
 
 		if (i != 2) {
 			printf(", ");
