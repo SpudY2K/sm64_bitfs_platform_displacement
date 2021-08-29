cfd461a663f16af02b9f450ae5910e0ccf3e7230
SpudY2K <87917486+SpudY2K@users.noreply.github.com>
2021-08-29 09:58:47 +0100

    Updated rotation method
    Previous method was modelled after Mario's platform displacement, which isn't completely accurate. New method mirrors how platforms are rotated in decomp.

--- Surface.cpp
+++ Surface.cpp
@@ -1,40 +1,21 @@
 #include "Surface.hpp"
 
-void Surface::rotate(const Vec3s& pivot, const Mat4& old_mat, const Mat4& new_mat) {
-	Vec3f dist1;
-	Vec3f dist2;
-	Vec3f dist3;
-	Vec3f posBeforeRotation1;
-	Vec3f posAfterRotation1;
-	Vec3f posBeforeRotation2;
-	Vec3f posAfterRotation2;
-	Vec3f posBeforeRotation3;
-	Vec3f posAfterRotation3;
+void Surface::rotate(const Mat4& m) {
+	reset_vectors();
 
-	for(int i = 0; i < 3; i++) {
-		dist1[i] = vectors[0][i] - pivot[i];
-		dist2[i] = vectors[1][i] - pivot[i];
-		dist3[i] = vectors[2][i] - pivot[i];
+	for (int i = 0; i < 3; i++) {
+		float vx = vectors[i][0];
+		float vy = vectors[i][1];
+		float vz = vectors[i][2];
+
+		vectors[i][0] = (short)(int)(vx * m[0][0] + vy * m[1][0] + vz * m[2][0] + m[3][0]);
+		vectors[i][1] = (short)(int)(vx * m[0][1] + vy * m[1][1] + vz * m[2][1] + m[3][1]);
+		vectors[i][2] = (short)(int)(vx * m[0][2] + vy * m[1][2] + vz * m[2][2] + m[3][2]);
 	}
 
-	linear_mtxf_mul_vec3f(posBeforeRotation1, old_mat, dist1);
-	linear_mtxf_mul_vec3f(posAfterRotation1, new_mat, dist1);
-
-	linear_mtxf_mul_vec3f(posBeforeRotation2, old_mat, dist2);
-	linear_mtxf_mul_vec3f(posAfterRotation2, new_mat, dist2);
-
-	linear_mtxf_mul_vec3f(posBeforeRotation3, old_mat, dist3);
-	linear_mtxf_mul_vec3f(posAfterRotation3, new_mat, dist3);
-
-	for(int i = 0; i < 3; i++) {
-		vectors[0][i] += posAfterRotation1[i] - posBeforeRotation1[i];
-		vectors[1][i] += posAfterRotation2[i] - posBeforeRotation2[i];
-		vectors[2][i] += posAfterRotation3[i] - posBeforeRotation3[i];
-	}
-
-	normal[0] = (((vectors[1][1] - vectors[0][1]) * (vectors[2][2] - vectors[1][2])) - ((vectors[1][2] - vectors[0][2]) * (vectors[2][1] - vectors[1][1]))) * 1000.0f;
-	normal[1] = (((vectors[1][2] - vectors[0][2]) * (vectors[2][0] - vectors[1][0])) - ((vectors[1][0] - vectors[0][0]) * (vectors[2][2] - vectors[1][2]))) * 1000.0f;
-	normal[2] = (((vectors[1][0] - vectors[0][0]) * (vectors[2][1] - vectors[1][1])) - ((vectors[1][1] - vectors[0][1]) * (vectors[2][0] - vectors[1][0]))) * 1000.0f;
+	normal[0] = ((vectors[1][1] - vectors[0][1]) * (vectors[2][2] - vectors[1][2])) - ((vectors[1][2] - vectors[0][2]) * (vectors[2][1] - vectors[1][1]));
+	normal[1] = ((vectors[1][2] - vectors[0][2]) * (vectors[2][0] - vectors[1][0])) - ((vectors[1][0] - vectors[0][0]) * (vectors[2][2] - vectors[1][2]));
+	normal[2] = ((vectors[1][0] - vectors[0][0]) * (vectors[2][1] - vectors[1][1])) - ((vectors[1][1] - vectors[0][1]) * (vectors[2][0] - vectors[1][0]));
 
 	vec3f_normalize(normal);
 
