c7232509cea8436c3c3c75cef47a8745c87d29a0
SpudY2K <87917486+SpudY2K@users.noreply.github.com>
2021-08-29 10:05:42 +0100

    Changed ground step calculations
    X and Z velocities are now precalculated and applied individually on each ground step, which matches closer with decomp.

--- Mario.cpp
+++ Mario.cpp
@@ -10,12 +10,15 @@ int Mario::ground_step(Vec2S& triangles) {
 	const Surface* floor = find_floor(pos, triangles);
 
 	if (floor) {
+		float x_vel = this->speed * gSineTable[uint16_t(this->yaw) >> 4];
+		float z_vel = this->speed * gCosineTable[uint16_t(this->yaw) >> 4];
+
 		for (int i = 0; i < 4; i++) {
 			float old_x = this->pos[0];
 			float old_z = this->pos[2];
 
-			this->pos[0] = this->pos[0] + gSineTable[uint16_t(this->yaw) >> 4] * floor->normal[1] * (this->speed / 4.0f);
-			this->pos[2] = this->pos[2] + gCosineTable[uint16_t(this->yaw) >> 4] * floor->normal[1] * (this->speed / 4.0f);
+			this->pos[0] = this->pos[0] + floor->normal[1] * (x_vel / 4.0f);
+			this->pos[2] = this->pos[2] + floor->normal[1] * (z_vel / 4.0f);
 
 			short x_mod = (short)(int)this->pos[0];
 			short y_mod = (short)(int)this->pos[1];
