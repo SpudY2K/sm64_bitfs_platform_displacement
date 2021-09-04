280ad09ac3ac651a4e44314177136d7cb4edccdb
SpudY2K <87917486+SpudY2K@users.noreply.github.com>
2021-09-04 18:59:24 +0100

    Added floor height to find_floor

--- Magic.hpp
+++ Magic.hpp
@@ -17,6 +17,6 @@ pair<int16_t, float> calc_intended_yawmag(int8_t stick_x, int8_t stick_y);
 bool check_inbounds(const Mario &m);
 float dist_calc(const Vec3f& x, const Vec3f& y);
 float line_point(const Vec3s& p1, const Vec3s& p2, float x, bool yOrZ);
-Surface const * find_floor(Vec3f& pos, Vec2S& triangles);
+Surface const * find_floor(Vec3f& pos, Vec2S& triangles, float* height);
 
 #endif
